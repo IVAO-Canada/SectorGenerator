a96de3fa2396e3ced2dbca5e54d4530bfb422ec1
Wes Sleeman <weston@wsleeman.com>
2024-05-13 23:23:33 -0700

    Added worldsector generator.

---
+++ WorldsectorGenerator/Structures.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WorldsectorGenerator;
+
+internal record Point([property: JsonPropertyName("lat")] double Latitude, [property: JsonPropertyName("lng")]  double Longitude) { }
+
+internal record Boundary(Point[] Points) { }
