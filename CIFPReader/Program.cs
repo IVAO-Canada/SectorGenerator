0c536575c019bbb04ec45e3d5be3db43c8a5d509
Wes Sleeman <weston.sleeman@ivao.aero>
2024-11-13 20:30:33 -0800

    Fixed CIFPReader output directory.

--- CIFPReader/Program.cs
+++ CIFPReader/Program.cs
@@ -2,5 +2,5 @@
 
 Console.WriteLine("Loading CIFPs.");
 DateTimeOffset start = DateTimeOffset.UtcNow;
-_ = CIFP.Load("cifps");
+_ = CIFP.Load("");
 Console.WriteLine($"Loading completed in {(DateTimeOffset.UtcNow - start).TotalSeconds:0.000} seconds.");
