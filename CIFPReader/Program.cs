826c85a16ebf492de2e33ae6c8199a29d7b4e86c
Wes Sleeman <weston.sleeman@ivao.aero>
2024-11-13 21:20:12 -0800

    Added additional logging.

--- CIFPReader/Program.cs
+++ CIFPReader/Program.cs
@@ -9,9 +9,15 @@ DateTimeOffset processed = DateTimeOffset.UtcNow;
 Console.WriteLine($"Loading completed in {(processed - start).TotalSeconds:0.000} seconds.");
 AmazonS3Client s3 = new();
 foreach (string filename in Directory.EnumerateFiles("cifp"))
-	await s3.PutObjectAsync(new() {
+{
+	CancellationTokenSource cts = new(TimeSpan.FromSeconds(20));
+	Console.WriteLine($"Uploading {filename}...");
+	_ = await s3.PutObjectAsync(new() {
 		BucketName = "ivao-xa",
 		Key = Path.GetFileName(filename),
-		FilePath = filename,
-	});
+		FilePath = Path.GetFullPath(filename),
+		ContentType = "application/json",
+	}, cts.Token);
+	Console.WriteLine("Done!");
+}
 Console.WriteLine($"Uploading completed in {(DateTimeOffset.UtcNow - processed).TotalSeconds:0.000} seconds.");
