95522f8d2871ee2a63db1c42f95593dd490e0e4c
Wes Sleeman <weston.sleeman@ivao.aero>
2024-11-13 21:51:38 -0800

    Converted upload from text to TransferUtility.

--- CIFPReader/Program.cs
+++ CIFPReader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Amazon.S3;
+using Amazon.S3.Transfer;
 
 using CIFPReader;
 
@@ -8,16 +9,6 @@ _ = CIFP.Load("");
 DateTimeOffset processed = DateTimeOffset.UtcNow;
 Console.WriteLine($"Loading completed in {(processed - start).TotalSeconds:0.000} seconds.");
 AmazonS3Client s3 = new();
-foreach (string filename in Directory.EnumerateFiles("cifp"))
-{
-	CancellationTokenSource cts = new(TimeSpan.FromSeconds(30));
-	Console.WriteLine($"Uploading {filename} to s3://ivao-xa/{Path.GetFileName(filename)}...");
-	_ = await s3.PutObjectAsync(new() {
-		BucketName = "ivao-xa",
-		Key = Path.GetFileName(filename),
-		ContentBody = File.ReadAllText(filename),
-		ContentType = "application/json",
-	}, cts.Token);
-	Console.WriteLine("Done!");
-}
+TransferUtility transfer = new(s3);
+await transfer.UploadDirectoryAsync("cifp", "ivao-xa", "*.json", SearchOption.TopDirectoryOnly);
 Console.WriteLine($"Uploading completed in {(DateTimeOffset.UtcNow - processed).TotalSeconds:0.000} seconds.");
