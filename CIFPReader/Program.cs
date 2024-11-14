25c3369465a2afe71d2b81bac016c682ddeb02d4
Wes Sleeman <weston.sleeman@ivao.aero>
2024-11-13 21:15:13 -0800

    Made CIFP reader write to S3 directly.

--- CIFPReader/Program.cs
+++ CIFPReader/Program.cs
@@ -1,6 +1,17 @@
-﻿using CIFPReader;
+﻿using Amazon.S3;
+
+using CIFPReader;
 
 Console.WriteLine("Loading CIFPs.");
 DateTimeOffset start = DateTimeOffset.UtcNow;
 _ = CIFP.Load("");
-Console.WriteLine($"Loading completed in {(DateTimeOffset.UtcNow - start).TotalSeconds:0.000} seconds.");
+DateTimeOffset processed = DateTimeOffset.UtcNow;
+Console.WriteLine($"Loading completed in {(processed - start).TotalSeconds:0.000} seconds.");
+AmazonS3Client s3 = new();
+foreach (string filename in Directory.EnumerateFiles("cifp"))
+	await s3.PutObjectAsync(new() {
+		BucketName = "ivao-xa",
+		Key = Path.GetFileName(filename),
+		FilePath = filename,
+	});
+Console.WriteLine($"Uploading completed in {(DateTimeOffset.UtcNow - processed).TotalSeconds:0.000} seconds.");
