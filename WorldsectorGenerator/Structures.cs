65a3d42108255434cb961c6dcf82a73103705efb
Wes Sleeman <weston@wsleeman.com>
2024-11-19 22:17:14 -0800

    Cleaned up unused projects.

--- WorldsectorGenerator/Structures.cs
+++
@@ -1,7 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace WorldsectorGenerator;
-
-internal record Point([property: JsonPropertyName("lat")] double Latitude, [property: JsonPropertyName("lng")]  double Longitude) { }
-
-internal record Boundary(Point[] Points) { }
